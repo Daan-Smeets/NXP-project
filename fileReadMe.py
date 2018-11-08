d071775f20cbb7f0fab605ea795c61e85f0c5bef
Daan-Smeets <43410353+Daan-Smeets@users.noreply.github.com>
2018-11-08 08:57:08 +0100

    Add files via upload
    Comments

--- fileReadMe.py
+++ fileReadMe.py
@@ -1,7 +1,8 @@
 import xlsxwriter
 
+
 def findLine(input_file_name, to_find):
-    file = open(input_file_name, 'r')
+    file = open(input_file_name, 'r')  # open the file as readonly
 
     total_found_lines = 0
 
@@ -11,16 +12,16 @@ def findLine(input_file_name, to_find):
     description_list = []
 
     for curr_line in file:
-        if to_find in curr_line:
-            if ".i2cMap." in curr_line: #TODO: FIX to_find AND REMOVE .i2cMap
+        if to_find in curr_line:  # for each line with 'register.' in it, check for '.i2cMap.' and '.write'
+            if ".i2cMap." in curr_line:
                 total_found_lines += 1
                 sweep_list.append(False)
 
-                toFindIndex = curr_line.find((to_find))
-                output_line = curr_line[toFindIndex:]
-                output_line = output_line.replace(to_find, "")
+                to_find_index = curr_line.find(to_find)  # find and remove the part of the sentence with 'register.i2cMap.'
+                output_line = curr_line[to_find_index:]
+                output_line = output_line.replace((to_find + "i2cMap."), "")
 
-                if "//" in output_line:
+                if "//" in output_line:  # check for and save comments
                     comment_index = output_line.find("//")
                     description = output_line[comment_index + 3:]
                     description_list.append(description)
@@ -28,8 +29,8 @@ def findLine(input_file_name, to_find):
                 else:
                     description_list.append("")
 
-                if "=" in output_line:  # Ask why there are lines without '=' (i2cMap.man_state.markForExpect ( 0x9 );)
-                    equal_index = output_line.find("=")
+                if "=" in output_line:  # check and save commands/values
+                    equal_index = output_line.find("=")  # ask why there are lines without '=' (i2cMap.man_state.markForExpect ( 0x9 );)
                     semicolon_index = output_line.find(";")
                     command = output_line[:equal_index - 1]
                     value = output_line[equal_index + 2: semicolon_index]
@@ -38,32 +39,33 @@ def findLine(input_file_name, to_find):
                 else:
                     total_found_lines -= 1
                 print(output_line, end="")
-            elif ".write" in curr_line:
+            elif ".write" in curr_line: # if .write is found, make sure the line gets a 'FLUSH'
                 sweep_list.append(True)
 
-    excel("testStatesMy.xlsx", total_found_lines, command_list, value_list, sweep_list, description_list)
+    excel("TestStatesMy.xlsx", total_found_lines, command_list, value_list, sweep_list, description_list)
 
     file.close()
+
 
 def excel(file_name, total_found_lines, command_list, value_list, sweep_list, description_list):
     row = 0
     col = 0
 
-    workbook = xlsxwriter.Workbook(file_name)
-    test_state_labels = workbook.add_worksheet("test_state_labels")
+    workbook = xlsxwriter.Workbook(file_name)  # make the file
 
+    test_state_labels = workbook.add_worksheet("test_state_labels")  # make the sheets
     patroon_1 = workbook.add_worksheet("Patroon_1")
 
-    blue_row = workbook.add_format({"font_color": "white", "bg_color": "#4774c5"})
+    blue_row = workbook.add_format({"font_color": "white", "bg_color": "#4774c5"})  # the colors for the top row
 
+    # fill the first (standard) rows
     test_state_labels.write(0, 0, "Label Name", blue_row)
     test_state_labels.write(0, 1, "Protocol", blue_row)
-    test_state_labels.write(0, 2, "Settings" ,blue_row)
+    test_state_labels.write(0, 2, "Settings", blue_row)
     test_state_labels.write(0, 3, "Concurrent", blue_row)
 
     test_state_labels.write(1, 0, "Patroon_1")
     test_state_labels.write(1, 1, "I2C")
-
 
     patroon_1.write(0, 0, "Command", blue_row)
     patroon_1.write(0, 1, "Value", blue_row)
@@ -71,15 +73,16 @@ def excel(file_name, total_found_lines, command_list, value_list, sweep_list, de
     patroon_1.write(0, 3, "Sweep", blue_row)
     patroon_1.write(0, 4, "Description", blue_row)
 
-    for i in range(0, total_found_lines):  # patroon_1.write(, , )
+    # fill the rows/columns with the variables made in the other function
+    for i in range(0, total_found_lines):
         col = 0
         row += 1
 
         patroon_1.write(row, col, command_list[i])
         col += 1
         patroon_1.write(row, col, value_list[i])
-        col += 2
-        if sweep_list[i] == True:
+        col += 2  # ask if we need to do anything with the 'Expect' column
+        if sweep_list[i]:
             patroon_1.write(row, col, "FLUSH")
         col += 1
         patroon_1.write(row, col, description_list[i])
@@ -87,6 +90,5 @@ def excel(file_name, total_found_lines, command_list, value_list, sweep_list, de
 
     workbook.close()
 
+
 findLine("Bandgap.cpp", "registers.")
-
-
